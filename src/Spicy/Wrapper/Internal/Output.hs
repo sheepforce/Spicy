b573ee825ccd05867ca7bfb5c2a597b8ff375a8b
Phillip Seeber <phillip.seeber@uni-jena.de>
2019-12-04 13:12:01 +0100

    Comment out unused module at the moment

--- src/Spicy/Wrapper/Internal/Output.hs
+++ src/Spicy/Wrapper/Internal/Output.hs
@@ -11,9 +11,10 @@ This module provides parsers for wrapper outputs to obtain molecular information
 gradients and hessians.
 -}
 module Spicy.Wrapper.Internal.Output
-  ( getResultsFromOutput
+  ( --getResultsFromOutput
   )
 where
+{-
 import           Control.Applicative
 import           Control.Exception.Safe
 import           Control.Lens
@@ -24,13 +25,16 @@ import qualified Data.Text.Lazy                as T
 import           Debug.Trace
 import           Spicy.Generic
 import           Spicy.Wrapper.Internal.Types.Shallow
+-}
 
 {-|
 A parser for energies from Psi4 calculations. This parser tries to be as aware as possible of the
 calculation niveau.
 -}
+{-
 getResultsFromOutput :: MonadThrow m => Text -> WrapperInput -> m WrapperOutput
 getResultsFromOutput content wrapperInput
   | software == Psi4 = undefined
   | otherwise        = fail "No parser available for chosen software."
   where software = wrapperInput ^. wrapperInput_Software
+-}
