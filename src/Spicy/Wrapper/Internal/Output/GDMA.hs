b918a228d2bb161a3af2142c9be974b8e526d70f
Phillip Seeber <phillip.seeber@uni-jena.de>
2020-12-07 12:35:57 +0100

    Intermediate for GDMA parser rewrite

--- src/Spicy/Wrapper/Internal/Output/GDMA.hs
+++ src/Spicy/Wrapper/Internal/Output/GDMA.hs
@@ -17,10 +17,12 @@ where
 import Control.Applicative
 import Data.Attoparsec.Text
 import Data.Char
-import RIO
+import RIO hiding (take)
+import qualified RIO.Map as Map
+import qualified RIO.Text as Text
 import Spicy.Common
 import Spicy.Data
-import Spicy.Molecule
+import Spicy.Molecule hiding (C, S)
 
 -- | Parses all multipole expansion centres from the GDMA output as a list. The second part of the
 -- tuple is the multipole at the origin.
@@ -37,6 +39,19 @@ gdmaMultipoles = do
   return multipoleCentres
 
 ----------------------------------------------------------------------------------------------------
+
+-- | A data type to hold the parser results of GDMA in an ordered fashion.
+data MultipoleIdx = MultipoleIdx
+  { rank :: Int,
+    idx :: SphTIdx
+  }
+  deriving (Eq, Ord)
+
+data SphTIdx = SphTIdx Int ComponentL
+  deriving (Eq, Ord)
+
+data ComponentL = None | S | C
+  deriving (Eq, Ord)
 
 -- | Parse all multipole data of an expansion centre.
 multipoleCentre :: Parser Multipoles
@@ -58,179 +73,14 @@ multipoleCentre = do
       <* endOfLine
 
   -- Rank 0 - Monopoles
-  q0 <-
-    if (not $ maxRank >= 0)
-      then return Nothing
-      else do
-        q00' <- labelField "Q00" <* endOfLine
-        return . Just $ Monopole {q00 = q00'}
+  q0 <- do
+    (mpIdx, q00') <- skipHorizontalSpace *> labeledComponent
+    case mpIdx of
+      MultipoleIdx {rank = 0, idx = SphTIdx 0 None} -> return . Just $ Monopole {q00 = q00'}
+      _ -> fail "Rank 0 (monopole) could not be parsed correctly."
 
-  -- Rank 1 - Dipoles
-  q1 <-
-    if (not $ maxRank >= 1)
-      then return Nothing
-      else do
-        _q1Mag <- labelField "|Q1|"
-        q11c' <- labelField "Q11c"
-        q11s' <- labelField "Q11s" <* endOfLine
-        return . Just $
-          Dipole
-            { q11c = q11c',
-              q11s = q11s'
-            }
-
-  -- Rank 2 - Quadrupoles
-  q2 <-
-    if (not $ maxRank >= 2)
-      then return Nothing
-      else do
-        _q2Mag <- labelField "|Q2|"
-        q20' <- labelField "Q20"
-        q22c' <- labelField "Q22c"
-        q22s' <- labelField "Q22s" <* endOfLine
-        return . Just $
-          Quadrupole
-            { q20 = q20',
-              q22c = q22c',
-              q22s = q22s'
-            }
-
-  -- Rank 3 - Octopole
-  q3 <-
-    if (not $ maxRank >= 3)
-      then return Nothing
-      else do
-        _q3Mag <- labelField "|Q3|"
-        q31c' <- labelField "Q31c"
-        q31s' <- labelField "Q31s"
-        q33c' <- labelField "Q33c" <* endOfLine
-        q33s' <- labelField "Q33s" <* endOfLine
-        return . Just $
-          Octopole
-            { q31c = q31c',
-              q31s = q31s',
-              q33c = q33c',
-              q33s = q33s'
-            }
-
-  -- Rank 4 - Hexadecapole
-  q4 <-
-    if (not $ maxRank >= 4)
-      then return Nothing
-      else do
-        _q4Mag <- labelField "|Q4|"
-        q40' <- labelField "Q40"
-        q42c' <- labelField "Q42c"
-        q42s' <- labelField "Q42s" <* endOfLine
-        q44c' <- labelField "Q44c"
-        q44s' <- labelField "Q44s" <* endOfLine
-        return . Just $
-          Hexadecapole
-            { q40 = q40',
-              q42c = q42c',
-              q42s = q42s',
-              q44c = q44c',
-              q44s = q44s'
-            }
-
-  -- Rank 5
-  _q5 <-
-    if (not $ maxRank >= 5)
-      then return Nothing
-      else do
-        _q5Mag <- labelField "|Q5|"
-        _q51c' <- labelField "Q51c"
-        _q51s' <- labelField "Q51s"
-        _q53c' <- labelField "Q53c" <* endOfLine
-        _q53s' <- labelField "Q53s"
-        _q55c' <- labelField "Q55c"
-        _q55s' <- labelField "Q55s" <* endOfLine
-        return Nothing
-
-  -- Rank 6
-  _q6 <-
-    if (not $ maxRank >= 6)
-      then return Nothing
-      else do
-        _q6Mag <- labelField "|Q6|"
-        _q60' <- labelField "Q60"
-        _q62c' <- labelField "Q62c"
-        _q62s' <- labelField "Q62s" <* endOfLine
-        _q64c' <- labelField "Q64c"
-        _q64s' <- labelField "Q64s"
-        _q66c' <- labelField "Q66c" <* endOfLine
-        _q66s' <- labelField "Q66s" <* endOfLine
-        return Nothing
-
-  -- Rank 7
-  _q7 <-
-    if (not $ maxRank >= 7)
-      then return Nothing
-      else do
-        _q7Mag <- labelField "|Q7|"
-        _q71c' <- labelField "Q71c"
-        _q71s' <- labelField "Q71s"
-        _q73c' <- labelField "Q73c" <* endOfLine
-        _q73s' <- labelField "Q73s"
-        _q75c' <- labelField "Q75c"
-        _q75s' <- labelField "Q75s" <* endOfLine
-        _q77c' <- labelField "Q77c"
-        _q77s' <- labelField "Q77s" <* endOfLine
-        return Nothing
-
-  -- Ranke 8
-  _q8 <-
-    if (not $ maxRank >= 8)
-      then return Nothing
-      else do
-        _q8Mag <- labelField "|Q8|"
-        _q80' <- labelField "Q80"
-        _q82c' <- labelField "Q82c"
-        _q82s' <- labelField "Q82s" <* endOfLine
-        _q84c' <- labelField "Q84c"
-        _q84s' <- labelField "Q84s"
-        _q86c' <- labelField "Q86c" <* endOfLine
-        _q86s' <- labelField "Q86s"
-        _q88c' <- labelField "Q88c"
-        _q88s' <- labelField "Q88s" <* endOfLine
-        return Nothing
-
-  -- Rank 9
-  _q9 <-
-    if (not $ maxRank >= 9)
-      then return Nothing
-      else do
-        _q9Mag <- labelField "|Q9|"
-        _q91c' <- labelField "Q91c"
-        _q91s' <- labelField "Q91s"
-        _q93c' <- labelField "Q93c" <* endOfLine
-        _q93s' <- labelField "Q93s"
-        _q95c' <- labelField "Q95c"
-        _q95s' <- labelField "Q95s" <* endOfLine
-        _q97c' <- labelField "Q97c"
-        _q97s' <- labelField "Q97s"
-        _q99c' <- labelField "Q99c" <* endOfLine
-        _q99s' <- labelField "Q99s" <* endOfLine
-        return Nothing
-
-  -- Rank 10
-  _q10 <-
-    if (not $ maxRank >= 10)
-      then return Nothing
-      else do
-        _qAMag <- labelField "|QA|"
-        _qA0' <- labelField "QA0"
-        _qA2c' <- labelField "QA2c"
-        _qA2s' <- labelField "QA2s" <* endOfLine
-        _qA4c' <- labelField "QA4c"
-        _qA4s' <- labelField "QA4s"
-        _qA6c' <- labelField "QA6c" <* endOfLine
-        _qA6s' <- labelField "QA6s"
-        _qA8c' <- labelField "QA8c"
-        _qA8s' <- labelField "QA8s" <* endOfLine
-        _qAAc' <- labelField "QAAc"
-        _qAAs' <- labelField "QAAs" <* endOfLine
-        return Nothing
+  -- Other ranks.
+  multipoles <- Map.unions <$> many' rankBlock
 
   -- Expansion centres are separated by a blank line.
   endOfLine
@@ -238,13 +88,39 @@ multipoleCentre = do
   return
     Multipoles
       { monopole = q0,
-        dipole = q1,
-        quadrupole = q2,
-        octopole = q3,
-        hexadecapole = q4
+        dipole = undefined,
+        quadrupole = undefined,
+        octopole = undefined,
+        hexadecapole = undefined
       }
   where
-    labelField :: Text -> Parser Double
-    labelField label =
-      skipHorizontalSpace *> string label *> skipHorizontalSpace *> char '=' *> skipHorizontalSpace
-        *> double <* skipHorizontalSpace
+    -- A helper function to feed the result of one parser into another.
+    nextParse :: Parser a -> Text -> Parser a
+    nextParse nextParser t = case parseOnly nextParser t of
+      Left err -> fail err
+      Right res -> return res
+
+    -- Parsing the magnitude field of a multipole. Looks like @|Qx|@, where x is the multipole rank.
+    -- Returns the rank and the magnitude of this multipole moment.
+    magnitudeField :: Parser (Int, Double)
+    magnitudeField = do
+      rank <- char '|' *> char 'Q' *> decimal <* char '|' <* char ' ' <* char '='
+      mag <- skipMany (char ' ') *> double
+      return (rank, mag)
+
+    -- Parses a labeled component field of a multipole. Gives @(Rank, (Component, s/c), Value) for
+    -- each field.
+    labeledComponent :: Parser (MultipoleIdx, Double)
+    labeledComponent = do
+      rank' <- char 'Q' *> take 1 >>= nextParse (decimal <|> (char 'A' *> pure 10))
+      cmpntN <- decimal <|> (char 'A' *> pure 10)
+      cmpntL <- option None $ ((char 's' *> pure S) <|> (char 'c' *> pure C))
+      value' <- skipHorizontalSpace *> char '=' *> skipHorizontalSpace *> double
+      return (MultipoleIdx {rank = rank', idx = SphTIdx cmpntN cmpntL}, value')
+
+    -- Parse an entire block of a rank /= 0.
+    rankBlock :: Parser (Map MultipoleIdx Double)
+    rankBlock = do
+      _ <- magnitudeField
+      assocMap <- Map.fromList <$> (many' $ skipHorizontalSpace *> labeledComponent <* skipSpace)
+      return assocMap
