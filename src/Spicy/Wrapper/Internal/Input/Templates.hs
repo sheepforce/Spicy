fcd3f75d6e34f2d701139c3571c82f972ffa5705
Fürst Plattner <sebastian.seidenath@uni-jena.de>
2021-05-07 17:10:27 +0200

    Smarter Typeclass

--- src/Spicy/Wrapper/Internal/Input/Templates.hs
+++ src/Spicy/Wrapper/Internal/Input/Templates.hs
@@ -10,8 +10,10 @@
 -- This module provides a framework to construct inputs for external
 -- quantum chemistry programs.
 module Spicy.Wrapper.Internal.Input.Templates
-  ( makeInput
-  ) where
+  ( makeInput,
+    HasWrapperInput,
+  )
+where
 
 {-
 Previously, Spicy used mustache templates to construct input files.
@@ -30,13 +32,13 @@ inspect it for correctness, modify it after construction, etc.
 import Control.Monad.Free
 import Optics
 import RIO hiding (Lens', lens, view, (^.), (^?))
-import RIO.Writer
 import qualified RIO.Text as Text
+import RIO.Writer
 import Spicy.Common
-import Spicy.Molecule.Internal.Types
 import Spicy.Molecule
+import Spicy.Molecule.Internal.Types
 
-makeInput :: (HasInput env, MonadThrow m) => ReaderT env m Text
+makeInput :: (HasWrapperInput env, MonadThrow m) => ReaderT env m Text
 makeInput = do
   mthisSoftware <- asks (^? softwareAF)
   thisSoftware <- maybe2MThrow (WrapperGenericException "makeInput" "Could not determine software!") mthisSoftware
@@ -55,7 +57,7 @@ makeInput = do
 -- instead of Lens'es. AffineFolds are essentially half a prism with just
 -- the 'preview' function -- which makes sense, since there's no sensible
 -- setters here. They can be used with the standard '^?' operator.
-class HasInput env where
+class HasWrapperInput env where
   chargeAF :: AffineFold env Int
   multAF :: AffineFold env Int
   methodAF :: AffineFold env GFN
@@ -66,25 +68,18 @@ class HasInput env where
   prefixAF :: AffineFold env String
   permaDirAF :: AffineFold env JDirPathAbs
 
--- This instance expects the complete molecule from the top level onwards. IMO, this is somewhat ugly.
--- In the future, we might be interested in reworking this somehow - at the very least, add
--- a newtype wrapper that differentiates the top level complete system from sub-molecules.
-instance HasInput (Molecule, CalcID) where
-  chargeAF = thisInputAF % #qMMMSpec % _QM % #charge
-  multAF = thisInputAF % #qMMMSpec % _QM % #mult
-  methodAF = thisInputAF % #software % _XTB
-  taskAF = thisInputAF % #task
-  softwareAF = thisInputAF % #software
-  memoryAF = thisInputAF % #memory
-  moleculeAF = thisMolAF % _2
-  prefixAF = thisInputAF % #prefixName
-  permaDirAF = thisInputAF % #permaDir
-
-thisMolAF :: AffineFold (Molecule, CalcID) (CalcContext, Molecule)
-thisMolAF = afolding $ uncurry getCalcByID
-
-thisInputAF :: AffineFold (Molecule, CalcID) CalcInput
-thisInputAF = thisMolAF % _1 % #input
+-- This instance expects the /current/ molecule layer, that is,
+-- the one for which the input will be prepared.
+instance HasWrapperInput (Molecule, CalcInput) where
+  chargeAF = castOptic @An_AffineFold _2 % #qMMMSpec % _QM % #charge
+  multAF = castOptic @An_AffineFold _2 % #qMMMSpec % _QM % #mult
+  methodAF = castOptic @An_AffineFold _2 % #software % _XTB
+  taskAF = castOptic @An_AffineFold _2 % #task
+  softwareAF = castOptic @An_AffineFold _2 % #software
+  memoryAF = castOptic @An_AffineFold _2 % #memory
+  moleculeAF = castOptic _1
+  prefixAF = castOptic @An_AffineFold _2 % #prefixName
+  permaDirAF = castOptic @An_AffineFold _2 % #permaDir
 
 -- | General getting action.
 gget ::
@@ -96,25 +91,25 @@ gget ::
   m a
 gget af str = asks (^? af) >>= maybe2MThrow (WrapperGenericException ("get" <> str) "Value could not be found!")
 
-getCharge :: (MonadReader env m, MonadThrow m, HasInput env) => m Int
+getCharge :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m Int
 getCharge = gget chargeAF "Charge"
 
-getMult :: (MonadReader env m, MonadThrow m, HasInput env) => m Int
+getMult :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m Int
 getMult = gget multAF "Mult"
 
-getMethod :: (MonadReader env m, MonadThrow m, HasInput env) => m GFN
+getMethod :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m GFN
 getMethod = gget methodAF "Method"
 
-getMemory :: (MonadReader env m, MonadThrow m, HasInput env) => m Int
+getMemory :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m Int
 getMemory = gget memoryAF "Memory"
 
-getMolecule :: (MonadReader env m, MonadThrow m, HasInput env) => m Molecule
+getMolecule :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m Molecule
 getMolecule = gget moleculeAF "Molecule"
 
-getPrefix :: (MonadReader env m, MonadThrow m, HasInput env) => m String
+getPrefix :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m String
 getPrefix = gget prefixAF "Prefix"
 
-getTask :: (MonadReader env m, MonadThrow m, HasInput env) => m WrapperTask
+getTask :: (MonadReader env m, MonadThrow m, HasWrapperInput env) => m WrapperTask
 getTask = gget taskAF "Tasks"
 
 {-
@@ -135,7 +130,7 @@ data XTBInput a
 
 -- | This function embodies the /logical/ structure of the input file,
 -- and produces a /data/ representation of said file.
-xtbInput :: (HasInput env, MonadReader env m, MonadThrow m) => m (Free XTBInput ())
+xtbInput :: (HasWrapperInput env, MonadReader env m, MonadThrow m) => m (Free XTBInput ())
 xtbInput = do
   chrg <- getCharge
   mult <- getMult
@@ -145,7 +140,8 @@ xtbInput = do
     liftF $ XTBCharge chrg ()
     liftF $ XTBNOpen nopen ()
     liftF $ XTBMethod mthd ()
-    -- TODO: Multipoles
+
+-- TODO: Multipoles
 
 -- | This function specifies how to serialize the information contained in the
 -- abstract input representation
@@ -177,7 +173,7 @@ data Psi4Input a
   | PsiMultipoles Text a
   deriving (Functor)
 
-psi4Input :: (HasInput env, MonadReader env m, MonadThrow m) => m (Free Psi4Input ())
+psi4Input :: (HasWrapperInput env, MonadReader env m, MonadThrow m) => m (Free Psi4Input ())
 psi4Input = do
   mem <- getMemory
   chrg <- getCharge
@@ -190,14 +186,15 @@ psi4Input = do
     liftF $ PsiMemory mem ()
     liftF $ PsiMolecule chrg mult molRepr ()
     liftF $ PsiSet "def2-svp" () -- Placeholder
-    (o,wfn) <- defaultDefine
+    (o, wfn) <- defaultDefine
     liftF $ PsiFCHK wfn prefix ()
     when (task == WTHessian) . liftF $ PsiHessian o ()
-    --TODO: add multipoles here
   where
+    --TODO: add multipoles here
+
     defaultDefine =
-      let (o,wfn) = ("o","wfn")
-      in liftF $ PsiDefine o wfn (o,wfn)
+      let (o, wfn) = ("o", "wfn")
+       in liftF $ PsiDefine o wfn (o, wfn)
 
 ----------------------------------------------------------------------------------------------------
 
